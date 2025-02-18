cf097f8bf825d4e0b61c085231260b8468ecfe1b
David Bonilla <dbonilla1988@gmail.com>
2025-02-18 02:32:34 +0100

    Add/Update config.json for local & sepolia addresses

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config(); // Make sure to import dotenv
 
 module.exports = {
   solidity: "0.8.9",
@@ -6,8 +7,10 @@ module.exports = {
     hardhat: {
       chainId: 31337,
     },
-    customNetwork: {
-      url: "http://localhost:8545", // You can keep this for your custom network
+    sepolia: {
+      url: process.env.REACT_APP_SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY], // an array of private keys
+      chainId: 11155111,
     },
   },
 };
